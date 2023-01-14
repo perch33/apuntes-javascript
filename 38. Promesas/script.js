5d86868bd0c10ebeb8de89010f52ecfbfed64abf
percy chuzon <percyrondan1995@gmail.com>
2023-01-13 22:07:41 -0500

    introduccion a promesas 1

---
+++ 38. Promesas/script.js
@@ -0,0 +1,3 @@
+//Una promesa es un objeto con 2 callbacks internos
+const users = [{ id: 1, name: 'Dorian' }, { id: 2, name: 'Laura' }, { id: 3, name: 'Carlos' }]
+const emails = [{ id: 1, email: 'dorian@gmail.com' }, { id: 2, email: 'laura@gmail.com' }]
