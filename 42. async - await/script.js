5b2f818bccb7dcc3a8e2f711d033b35e66b5c23c
percy chuzon <percyrondan1995@gmail.com>
2023-01-21 18:43:08 -0500

    async - await introduccion

--- 42. async - await/script.js
+++ 42. async - await/script.js
@@ -1,15 +1,19 @@
 /* const getName = async () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Dorian')
-        }, 1500)
-    })
-}
+  return new Promise((resolve, reject) => {
+    //para simular una consulta a una api o a una base de dato
+    setTimeout(() => {
+      resolve("Dorian");
+    }, 1500);
+  });
+};
+
 const sayHello = async () => {
-    const name = await getName()
-    return `Hello ${name}`
-}
-sayHello().then(res=>console.log(res)) */
+  const name = await getName();
+  return `Hello ${name}`;
+};
+sayHello().then((res) => console.log(res)); */
+
+/* await no funciona sin async porque genera un bloqueo hasta que no resuelva el código */
 
 const users = [
   { id: 1, name: "Dorian" },
@@ -20,3 +24,34 @@ const emails = [
   { id: 1, email: "dorian@gmail.com" },
   { id: 2, email: "laura@gmail.com" },
 ];
+
+const getUser = async (id) => {
+  const user = users.find((user) => user.id == id);
+  if (!user) throw new Error(`Doesn't exist an user with id ${id}`);
+  else return user;
+};
+
+const getEmail = async (user) => {
+  const email = emails.find((email) => email.id == user.id);
+  if (!email) throw new Error(`${user.name} hasn't email`);
+  else
+    return {
+      id: user.id,
+      name: user.name,
+      email: email.email,
+    };
+};
+
+const getInfo = async (id) => {
+  try {
+    const user = await getUser(id);
+    const res = await getEmail(user);
+    return `${user.name} email is ${res.email}`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getInfo(3).then((res) => console.log(res));
+
+
