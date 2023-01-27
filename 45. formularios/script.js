6d03ac1c636883cc5f0e19edb2796d5b7c26b267
percy chuzon <percyrondan1995@gmail.com>
2023-01-26 21:20:15 -0500

    introduccion a validacion de formularios

---
+++ 45. formularios/script.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("form");
+const button = document.getElementById("submitButton");
+
+const names = document.getElementById("name");
+const email = document.getElementById("email");
+const gender = document.getElementById("gender");
+const terms = document.getElementById("terms");
+
+const formIsValid = {
+  name: false,
+  email: false,
+  gender: false,
+  terms: false,
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+/*"change" registra el evento siempre y cuando el valor del campo haiga cambiado */
+names.addEventListener("change", (e) => {
+  if (e.target.value.trim().length > 0) {
+    formIsValid.name = true;
+  }
+});
+
+email.addEventListener("change", (e) => {
+  if (e.target.value.trim().length > 0) {
+    formIsValid.email = true;
+  }
+});
+gender.addEventListener("change", (e) => {
+  if ((e.target.checked = "true")) {
+    formIsValid.gender = true;
+  }
+});
+
+terms.addEventListener("change", (e) => {
+  formIsValid.terms = e.target.checked;
+  e.target.checked
+    ? button.removeAttribute("disabled")
+    : button.setAttribute("disabled", true);
+});
