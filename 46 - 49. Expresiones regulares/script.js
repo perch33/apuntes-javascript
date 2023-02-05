f2e17c32a62a99930bf54aaa37587eae3b1315ca
percy chuzon <percyrondan1995@gmail.com>
2023-02-04 20:04:43 -0500

    se agregaron nuevos apuntes

--- 46 - 49. Expresiones regulares/script.js
+++ 46 - 49. Expresiones regulares/script.js
@@ -24,28 +24,40 @@
             Podemos unir los dos anteriores en una sola expresión. [0-5ou] Serían números del 0 al 5, la letra "o" y la letra "u"
         -Cadenas completas:
             Para establecer una cadena completa debe ir entre paréntesis, si queremos más palabras irán separadas por un pipe. (lorem|amet) es válida la palabra "lorem" y la palabra "amet"
+
     Delimitadores:
         ^ Antes de este símbolo no puede haber nada
         $ Después de este símbolo no puede haber nada
-        ^hola$
+        ^hola$  //solo selecciona la palabra hola
+        hola? //no es seleccionado
+
     Cantidad:
         - llaves: lo que está antes tiene que aparecer la cantidad exacta de veces. Hay tres combinaciones posibles. 
             {n} Se tiene que repetir n veces
             {n,m} Se tiene que repetir entre n y m veces, ambas incluidas.
             {n,} Se tiene que repetir como mínimo n veces y sin máximo
         ^[a-zA-Z]{1,3}@{1}$
+
         -asterisco: Lo que está antes del asterisco puede estar, puede no estar y se puede repetir.  .*@.*\..*
-        -interrogación: Lo que está antes de la interrogación puede no estar, pero si está solo puede aparecer una vez.
+
+        -interrogación "?": Lo que está antes de la interrogación puede no estar, pero si está solo puede aparecer una vez.
             ^[ae]?$ 
+
         - operador +: lo que está antes del + tiene que estar una vez como mínimo
         A-[0-9]+
     
     Caracteres:
+    //en minúscula admite y mayuscula no admite
         \s: Coincide con un carácter de espacio, entre ellos incluidos espacio, tab, salto de página, salto de linea y retorno de carro. ^[a-zA-Z]+\s[a-zA-Z]+$
+
         \S: Coincide con todo menos caracteres de espacio ^\S{5}$
+
         \d: Coincide con un carácter de número. Equivalente a [0-9] ^\d{5}$
+
         \D: Coincide con cualquier carácter no numérico. Equivalente a [^0-9] ^\D{5}$
+
         \w: Coincide con cualquier carácter alfanumérico, incluyendo el guion bajo. Equivalente a [A-Za-z0-9_] ^\w+@$
+
         \W: Coincide con todo menos caracteres de palabra. ^\W+$
        
 */
@@ -59,4 +71,5 @@ const regEx = /lorem/gi;
 const regEx2 = new RegExp("lorem", "gi");
 const regEx3 = new RegExp("/lorem/", "gi");
 
-console.log(regEx.test(text));
+console.log(regEx.test(text)); //devuelve true o false
+console.log(text.includes("Lorem")); //obtendríamos el mismo resultado pero no es el adecuado
